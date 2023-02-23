0fc4b9818f7b69d9cbfd6dcd95dc74a75d3fbdd2
MartinaP87 <bleach1987@hotmail.it>
2023-02-23 21:48:56 +0000

    Add sign up and sign in forms.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import styles from './App.module.css';
+import Container from 'react-bootstrap/Container'
 import NavBar from './components/NavBar';
+import {Route, Switch} from 'react-router-dom'
+import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar/>
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/signin/" render={() => <SignInForm />} />
+          <Route exact path="/signup/" render={() => <SignUpForm />} />
+          <Route render={() => <p>Page Not Found</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,24 +2,46 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md"fixed="top">
+    <Navbar expand="md" fixed="top" className={styles.NavBar}>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <NavLink exact to="/">
+          <Navbar.Brand className={styles.NavbarBrand}>Only Events</Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
-            <Nav.Link>Feeds</Nav.Link>
-            <Nav.Link>Preferred</Nav.Link>
-            <Nav.Link>Interested</Nav.Link>
-            <Nav.Link>Going</Nav.Link>
-            <Nav.Link>Past Events</Nav.Link>
-            <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign In</Nav.Link>
-            <Nav.Link><i className="fas fa-user-plus"></i>Sign Up</Nav.Link>
-            <Nav.Link>Sign Out</Nav.Link>
+            <NavLink exact to="/" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-home"></i>
+              Home
+            </NavLink>
+            <NavLink to="/feeds" className={styles.NavLink} activeClassName={styles.Active}>
+              Feeds
+            </NavLink>
+            <NavLink to="/preferres" className={styles.NavLink} activeClassName={styles.Active}>
+              Preferred
+            </NavLink>
+            <NavLink to="/interested" className={styles.NavLink} activeClassName={styles.Active}>
+              Interested
+            </NavLink>
+            <NavLink to="/going" className={styles.NavLink} activeClassName={styles.Active}>
+              Going
+            </NavLink>
+            <NavLink to="/past" className={styles.NavLink} activeClassName={styles.Active}>
+              Past Events
+            </NavLink>
+            <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fas fa-sign-in-alt"></i>Sign In
+            </NavLink>
+            <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fas fa-user-plus"></i>Sign Up
+            </NavLink>
+            <NavLink to="/signout" className={styles.NavLink} activeClassName={styles.Active}>
+              Sign Out
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
