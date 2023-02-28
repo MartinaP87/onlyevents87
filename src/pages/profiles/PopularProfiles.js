8735f138811cc892a2363e302b811ec7f630ffbb
MartinaP87 <bleach1987@hotmail.it>
2023-02-28 17:14:56 +0000

    Add ProfileDataContext.

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,33 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
-import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useProfileData } from "../../contexts/ProfileDataContext";
 import Profile from "./Profile";
 
 const PopularProfiles = ({mobile}) => {
-  const [profileData, setProfileData] = useState({
-    pageProfile: { results: [] },
-    popularProfiles: { results: [] },
-  });
-  const { popularProfiles } = profileData;
-  const currentUser = useCurrentUser();
+  const { popularProfiles } = useProfileData();
 
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(
-          `/profiles/?ordering=-followers_count`
-        );
-        setProfileData((prevState) => ({
-          ...prevState,
-          popularProfiles: data,
-        }));
-      } catch (err) {}
-    };
-    handleMount();
-  }, [currentUser]);
   return (
     <Container className={`${appStyles.Content} ${mobile && "d-lg-none text-center mb-3"}`}>
       {popularProfiles.results.length ? (
