50fa7c56c6e354001f6407e6ba889c784913b68a
MartinaP87 <bleach1987@hotmail.it>
2023-02-28 10:22:33 +0000

    Create the CommentCreateForm component and insert it in EventPage.

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -1,39 +1,53 @@
 import React, { useEffect, useState } from "react";
-
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import Event from "./Event";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function EventPage() {
   const { id } = useParams();
-  const [event, setEvent] = useState({results :[] });
+  const [event, setEvent] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
-        try{
-            const [{data: event}] = await Promise.all([
-                axiosReq.get(`/events/${id}`),
-            ])
-            setEvent({results: [event]})
-            console.log(event)
-        } catch(err) {console.log(err)}
-    }
+      try {
+        const [{ data: event }] = await Promise.all([
+          axiosReq.get(`/events/${id}`),
+        ]);
+        setEvent({ results: [event] });
+        console.log(event);
+      } catch (err) {
+        console.log(err);
+      }
+    };
     handleMount();
-  }, [id])
-
+  }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <Event {...event.results[0]} setEvents={setEvent} eventPage/>
+        <Event {...event.results[0]} setEvents={setEvent} eventPage />
         <Container className={appStyles.Content}>
-          Comments
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              event={id}
+              setEvent={setEvent}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
